d6a9945b31acaff6d8a2d04c7e6572cd3929deb3
remus-wolico <135813551+remus00@users.noreply.github.com>
2024-12-16 14:30:10 +0100

    fix header z-index

--- src/components/sections/header.tsx
+++ src/components/sections/header.tsx
@@ -2,7 +2,7 @@ import Link from 'next/link';
 
 export const Header = () => {
     return (
-        <header className="relative top-3 flex items-center justify-center">
+        <header className="fixed left-1/2 top-3 z-10 flex -translate-x-1/2 items-center justify-center">
             <nav className="flex items-center gap-1 rounded-full border border-white/15 bg-white/10 p-0.5 backdrop-blur">
                 <Link href="#" className="nav-item">
                     Home
