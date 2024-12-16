0b4a4a521cbf72e6743e16ac26fc46f15ab5ca36
remus-wolico <135813551+remus00@users.noreply.github.com>
2024-12-16 11:25:14 +0100

    project setup

---
+++ tailwind.config.ts
@@ -0,0 +1,24 @@
+import type { Config } from 'tailwindcss';
+
+export default {
+    content: [
+        './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+        './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+        './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    ],
+    theme: {
+        screens: {
+            sm: '375px',
+            md: '768px',
+            lg: '1200px',
+        },
+        container: {
+            center: true,
+            padding: {
+                DEFAULT: '1rem',
+                md: '2rem',
+            },
+        },
+    },
+    plugins: [],
+} satisfies Config;
